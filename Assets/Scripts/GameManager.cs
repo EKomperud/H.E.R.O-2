3a447d1a200c19cfe66763e2314d0a673ce6071b
ukjolie2 <u1008240@bmac-14.scl.utah.edu>
2016-06-25 18:38:49 -0600

    Menu Sliding

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public bool MainMenuSlide = false;
+	public bool Back = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RestartLevel() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void ExitLevel (){
+		Application.LoadLevel (0);
+	}
+
+	public void PlayGame() {
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+	public void MainMenu() {
+		MainMenuSlide = true;
+	}
+	public void GoBack() {
+		Back = true;
+	}
+}
---
+++ Assets/Scripts/MenuSliding.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSliding : MonoBehaviour {
+
+	public Vector3 endPosition = Vector3.zero;
+	public float speed = 1f;
+
+	private float timer = 0f;
+	private Vector3 startPosition = Vector3.zero;
+	private bool outgoing = true;
+	public GameManager Menu; 
+
+
+	// Use this for initialization
+	void Start () {
+		startPosition = this.gameObject.transform.position;
+		endPosition = endPosition + startPosition;
+
+		float distance = Vector3.Distance (startPosition, endPosition);
+		if (distance != 0) {
+			speed = speed / distance;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime * speed;
+
+		if (outgoing) {
+			this.transform.position = Vector3.Lerp (startPosition, endPosition, timer);
+			if (Menu.MainMenuSlide) {
+				outgoing = false;
+				timer = 0;
+			}
+		} 
+		else {
+			this.transform.position = Vector3.Lerp (endPosition, startPosition, timer);
+			if (Menu.MainMenuSlide && Menu.Back) {
+				outgoing = true;
+				timer = 0;
+			}
+		}
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (this.transform.position, endPosition + this.transform.position);
+	}
+}
