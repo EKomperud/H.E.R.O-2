4f4f1a723d088b1b5dc05cccb661d008a98bdf89
ukjolie2 <u1008240@bmac-14.scl.utah.edu>
2016-06-25 20:58:06 -0600

    Got the play button functioning

--- Assets/Scripts/MenuSliding.cs
+++ Assets/Scripts/MenuSliding.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class MenuSliding : MonoBehaviour {
 
 	public Vector3 endPosition = Vector3.zero;
-	public float speed = 1f;
+	public float speed = 3f;
 
 	private float timer = 0f;
 	private Vector3 startPosition = Vector3.zero;
-	private bool outgoing = true;
-	public GameManager Menu; 
+	private GameManager Menu;
 
 
 	// Use this for initialization
 	void Start () {
+		Menu = GameObject.Find("GameManager").GetComponent<GameManager> ();
 		startPosition = this.gameObject.transform.position;
 		endPosition = endPosition + startPosition;
 
@@ -25,21 +25,20 @@ public class MenuSliding : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime * speed;
-
-		if (outgoing) {
-			this.transform.position = Vector3.Lerp (startPosition, endPosition, timer);
-			if (Menu.MainMenuSlide) {
-				outgoing = false;
-				timer = 0;
+		if (Menu.MainMenuSlide && Menu.Back) {
+			Debug.Log ("pieMajor");
+			timer += Time.deltaTime * speed;
+			this.transform.position = Vector3.Lerp (endPosition, startPosition, timer);
+			if (this.transform.position == startPosition) {
+				Debug.Log (this.transform.position);
+				Debug.Log (startPosition);
+				Menu.MainMenuSlide = false;
+				Menu.Back = false;
 			}
 		} 
-		else {
-			this.transform.position = Vector3.Lerp (endPosition, startPosition, timer);
-			if (Menu.MainMenuSlide && Menu.Back) {
-				outgoing = true;
-				timer = 0;
-			}
+		else if (Menu.MainMenuSlide) {
+			timer += Time.deltaTime * speed;
+			this.transform.position = Vector3.Lerp (startPosition, endPosition, timer);
 		}
 	}
 
