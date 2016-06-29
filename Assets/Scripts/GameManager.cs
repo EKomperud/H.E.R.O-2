80714d26a43d85dd18ea79f3ebba4c0c6631e68a
u1008240 <u1008240@bmac-6.scl.utah.edu>
2016-06-29 11:08:39 -0600

    Need to rewrite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
     public HealthScript p1;
     public Health2Script p2;
+	public SpikeScript spikes;
 	public bool MainMenuSlide = false;
 	public bool playBack = false;
 	public bool setTimer = false;
@@ -12,45 +13,74 @@ public class GameManager : MonoBehaviour {
 	private int numOfRounds = 0;
 	private int randomLevel = 0;
 	private bool startUp = false;
+	private bool P1exists = false;
+	private bool P2exists = false;
 
 	// Use this for initialization
 	void Start () {
 		//Keeper = GameObject.Find("RoundKeeper").GetComponent<RoundKeeper> ();
         p1 = gameObject.GetComponent<HealthScript>();
         p2 = gameObject.GetComponent<Health2Script>();
+		spikes = gameObject.GetComponent<SpikeScript> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (startUp) {
-			randomLevel = Random.Range (1, 9);
+			randomLevel = Random.Range (1, 8);
 			Application.LoadLevel (randomLevel);
 			numOfRounds -= 1;
 			startUp = false;
 		}
 		if (ifDied && numOfRounds > 0) {
-			randomLevel = Random.Range (1, 9);
+			randomLevel = Random.Range (1, 8);
 			Application.LoadLevel (randomLevel);
 			numOfRounds -= 1;
 			ifDied = false;
-		} 
-		else if (ifDied && numOfRounds <= 0) {
+		} else if (ifDied && numOfRounds <= 0) {
 			//setActive winscreen
 		}
-		if (randomLevel > 0) {
-			if (p1 == null && p2.shotsHaveFired) {
-				ifDied = true;
-				p2.shotsHaveFired = false;
+		if (p1 != null) {
+			P1exists = true;
+		}
+		if (p2 != null) {
+			P2exists = true;
+		}
+		if (p1 != null && p1.hp == 0) {
+			ifDied = true;
+		} 
+		else if (p2 != null && p2.hp == 0) {
+			ifDied = true;
+		}
+		if (P1exists) {
+			if (p1 == null) {
+				Debug.Log ("the code gets here");
+				if (p2.shotsHaveFired) {
+					ifDied = true;
+					p2.shotsHaveFired = false;
+					P1exists = false;
+				}
+				if (spikes != null) {
+					if (spikes.hasHit) {
+						ifDied = true;
+						P1exists = false;
+					}
+				}
 			}
-			else if (p2 == null && p1.shotsHaveFired) {
-				ifDied = true;
-				p1.shotsHaveFired = false;
-			}
-			if (p1 != null && p1.hp == 0) {
-				ifDied = true;
-			} 
-			else if (p2 != null && p2.hp == 0) {
-				ifDied = true;
+		}
+		else if (P2exists) {
+			if (p2 == null) {
+				if (p1.shotsHaveFired) {
+					ifDied = true;
+					p1.shotsHaveFired = false;
+					P2exists = false;
+				}
+				if (spikes != null) {
+					if (spikes.hasHit) {
+						ifDied = true;
+						P2exists = false;
+					}
+				}
 			}
 		}
 	}
--- Assets/Scripts/SpikeScript.cs
+++ Assets/Scripts/SpikeScript.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class SpikeScript : MonoBehaviour {
 
+	public bool hasHit = false;
+
 	// Use this for initialization
 	void OnTriggerEnter2D (Collider2D collider) {
 		Health2Script Player2 = collider.gameObject.GetComponent<Health2Script> ();
 		HealthScript Player1 = collider.gameObject.GetComponent<HealthScript> ();
 		if (Player1 != null) {
+			hasHit = true;
 			Destroy (Player1.gameObject);
 		}
 		if (Player2 != null) {
+			hasHit = true;
 			Destroy	(Player2.gameObject);
 		}
 	}
