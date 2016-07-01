32386d707f4fa62fa792e5332c5798351d4681c0
u1008240 <u1008240@1110-mac-05.lib.utah.edu>
2016-06-30 19:43:44 -0600

    Working on Menu again

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -5,7 +5,10 @@ public class HealthScript : MonoBehaviour {
 
 	public int hp = 5;
 	public bool shotsHaveFired = false;
+	public bool shotsHaveFired2 = false;
 	private PlayerController player; 
+	public bool death = false;
+	public bool death2 = false;
 
 	void Awake () {
 		player = gameObject.GetComponent<PlayerController> ();
@@ -15,13 +18,24 @@ public class HealthScript : MonoBehaviour {
 		//player = gameObject.GetComponent<PlayerScript>();
 		PlayerWeaponScript shot = collider.gameObject.GetComponent<PlayerWeaponScript> ();
         PlayerController target = shot.caster;
-		if (shot != null && !target.Equals(player)) {	
-			shotsHaveFired = true;
+		if (shot != null && !target.Equals(player)) {
+			if (this.gameObject.tag == "Player") {	
+				shotsHaveFired = true;
+			}
+			if (this.gameObject.tag == "Player2") {
+				shotsHaveFired2 = true;
+			}
 			hp -= shot.damage;
             System.Threading.Thread d = new System.Threading.Thread(() => shot.Destroy());
             d.Start();
 			//Destroy (shot.gameObject);
 			if (hp <= 0) {
+				if (this.gameObject.tag == "Player") {
+					death = true;
+				}
+				if (this.gameObject.tag == "Player2") {
+					death2 = true;
+				}
 				if (player.weapon != null) {
 					Destroy (player.weapon);
 				}
