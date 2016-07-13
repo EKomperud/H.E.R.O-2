76471f209fdfeda08e7939fbb11e11efab7d2c74
EKomperud <ericmkomperud@gmail.com>
2016-07-12 20:22:20 -0600

    small prefab changes

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -14,49 +14,53 @@ public class HealthScript : MonoBehaviour {
 		Manager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
 	}
 
-	void OnTriggerEnter2D (Collider2D collider) {
-		//player = gameObject.GetComponent<PlayerScript>();
-		PlayerWeaponScript shot = collider.gameObject.GetComponent<PlayerWeaponScript> ();
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        //player = gameObject.GetComponent<PlayerScript>();
+        PlayerWeaponScript shot = collider.gameObject.GetComponent<PlayerWeaponScript>();
 
-        
-        if (shot != null && shot.fire) {
-		if (hp <= 0)
-		{
-			if (this.gameObject.tag == "Player")
-			{
-				Manager.death = true;
-			}
-			if (this.gameObject.tag == "Player2")
-			{
-				Manager.death2 = true;
-			}
-			if (player.weapon != null)
-			{
-				Destroy(player.weapon);
-			}
-			Destroy(gameObject);
-		}
-        if (shot != null ) {
 
-            PlayerController target = shot.caster;
-            if (!target.Equals(player))
+        if (shot != null && shot.fire)
+        {
+            if (hp <= 0)
             {
-				Debug.Log ("the code gets here");
                 if (this.gameObject.tag == "Player")
                 {
-                    shotsHaveFired = true;
+                    Manager.death = true;
                 }
                 if (this.gameObject.tag == "Player2")
                 {
-                    shotsHaveFired2 = true;
+                    Manager.death2 = true;
                 }
-                hp -= shot.damage;
-                System.Threading.Thread d = new System.Threading.Thread(() => shot.Destroy());
-                d.Start();
-                //Destroy (shot.gameObject);
-            }	
-		}
-	}
+                if (player.weapon != null)
+                {
+                    Destroy(player.weapon);
+                }
+                Destroy(gameObject);
+            }
+            if (shot != null)
+            {
+
+                PlayerController target = shot.caster;
+                if (!target.Equals(player))
+                {
+                    Debug.Log("the code gets here");
+                    if (this.gameObject.tag == "Player")
+                    {
+                        shotsHaveFired = true;
+                    }
+                    if (this.gameObject.tag == "Player2")
+                    {
+                        shotsHaveFired2 = true;
+                    }
+                    hp -= shot.damage;
+                    System.Threading.Thread d = new System.Threading.Thread(() => shot.Destroy());
+                    d.Start();
+                    //Destroy (shot.gameObject);
+                }
+            }
+        }
+    }
 
     public void Death()
     {
