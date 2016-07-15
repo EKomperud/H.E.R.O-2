baef142ea15df921687eae8c9ece7ba7988d0f25
ollieschmolly <ollieschmolly@gmail.com>
2016-07-15 02:45:20 -0600

    4 Different Players

--- Assets/Scripts/NumberKeeper.cs
+++ Assets/Scripts/NumberKeeper.cs
@@ -7,14 +7,20 @@ public class NumberKeeper : MonoBehaviour {
 	public int previousRounds = 0;
 	public int P1WINS = 0;
 	public int P2WINS = 0;
-	public bool hasHit1 = false;
+    public int P3WINS = 0;
+    public int P4WINS = 0;
+    public bool hasHit1 = false;
 	public bool hasHit2 = false;
-	public bool death = false;
+    public bool hasHit3 = false;
+    public bool hasHit4 = false;
+    public bool death = false;
 	public bool death2 = false;
+    public bool death3 = false;
+    public bool death4 = false;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		DontDestroyOnLoad (this.gameObject);
 	}
 	
